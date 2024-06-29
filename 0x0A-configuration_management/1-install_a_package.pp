1549d654af348fd6e2046b2c18136f82e7174951
Babaoni147 <obahedwin@ymail.com>
2024-06-29 11:25:10 +0100

    Update

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,9 @@
 #Install an especific version of flask
-
 package { 'Flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
-
-package { 'Werkzeug':
+package {'Werkzeug':
   ensure   => '2.1.0',
-  provider => 'pip3',
+  provider => 'pip3'
 }
-
-
