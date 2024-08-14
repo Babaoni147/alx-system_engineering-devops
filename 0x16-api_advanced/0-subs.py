3e61b9bfcca2e662a54c8dcb81cbc163799d067e
Babaoni147 <obahedwin@ymail.com>
2024-08-14 14:24:54 +0100

    Update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
-
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {"User-Agent": "custom_user_agent"}
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 200:
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,23 +1,28 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Query Reddit API to determine subreddit sub count
+"""
+
 import requests
 
 
 def top_ten(subreddit):
-    """Fetches and prints the top ten hot posts from a given subreddit."""
+    """Request top ten hot posts of subreddit
+    from Reddit API
+    """
 
-    headers = {'User-Agent': "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
 
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
+    headers = {'User-Agent': user_agent}
 
-        if response.status_code == 200:
-            data = response.json()
-            posts = data['data']['children']
-            for post in posts:
-                print(post['data']['title'])
-        else:
-            print(None)
-    except requests.exceptions.RequestException:
-        print(None)
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        print('None')
+    else:
+        # load response unit from json
+        data = r.json()['data']
+        # extract list of pages
+        posts = data['children']
+        for post in posts:
+            print(post['data']['title'])
