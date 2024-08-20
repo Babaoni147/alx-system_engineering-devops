63ced7ae391889bc8d895a695f951d9234ff42cd
Babaoni147 <obahedwin@ymail.com>
2024-08-20 15:01:05 +0100

    All_Task

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,13 @@
+# Fix problem of high amount of requests
+# Change high request limite in /etc/default/nginx rom 15 to 4096
+
+exec {'replace':
+  provider => shell,
+  command  => 'sudo sed -i "s/ULIMIT=\"-n 15\"/ULIMIT=\"-n 4096\"/" /etc/default/nginx',
+  before   => Exec['restart'],
+}
+
+exec {'restart':
+  provider => shell,
+  command  => 'sudo service nginx restart',
+}
